b7d31f32537e9582a9ed64086687540f80e32e5a
Adınız Soyadınız <135762649+zeyneptanrivermis@users.noreply.github.com>
2025-03-25 14:24:02 +0300

    Creating home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< Updated upstream
 
 const routes: Routes = [];
+=======
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CorporateLoginPageComponent } from './pages/corporate-login-page/corporate-login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'corporate-login', component: CorporateLoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'home', component: HomePageComponent },
+];
+>>>>>>> Stashed changes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
